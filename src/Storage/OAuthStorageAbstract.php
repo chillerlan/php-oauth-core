b9d645bc564a6f4e0c3112f02350e400c19eedea
codemasher <smiley@chillerlan.net>
2019-07-26 18:47:37 +0200

    :octocat: allow psr logger on storage interface

--- src/Storage/OAuthStorageAbstract.php
+++ src/Storage/OAuthStorageAbstract.php
@@ -14,8 +14,10 @@ namespace chillerlan\OAuth\Storage;
 
 use chillerlan\OAuth\{Core\AccessToken, OAuthOptions};
 use chillerlan\Settings\SettingsContainerInterface;
+use Psr\Log\{LoggerAwareInterface, LoggerAwareTrait, LoggerInterface, NullLogger};
 
-abstract class OAuthStorageAbstract implements OAuthStorageInterface{
+abstract class OAuthStorageAbstract implements OAuthStorageInterface, LoggerAwareInterface{
+	use LoggerAwareTrait;
 
 	/**
 	 * @var \chillerlan\OAuth\OAuthOptions
@@ -26,9 +28,12 @@ abstract class OAuthStorageAbstract implements OAuthStorageInterface{
 	 * OAuthStorageAbstract constructor.
 	 *
 	 * @param \chillerlan\Settings\SettingsContainerInterface|null $options
+	 * @param \Psr\Log\LoggerInterface|null                        $logger
 	 */
-	public function __construct(SettingsContainerInterface $options = null){
+	public function __construct(SettingsContainerInterface $options = null, LoggerInterface $logger = null){
 		$this->options = $options ?? new OAuthOptions;
+
+		$this->setLogger($logger ?? new NullLogger);
 	}
 
 	/**
