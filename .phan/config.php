9f6de524f16f332cdb4d8b6d16e079085145091b
codemasher <smiley@chillerlan.net>
2021-04-13 00:43:13 +0200

    :shower: remove examples from phan cfg

--- .phan/config.php
+++ .phan/config.php
@@ -24,7 +24,6 @@ return [
 	// Thus, both first-party and third-party code being used by
 	// your application should be included in this list.
 	'directory_list' => [
-		'examples',
 		'src',
 		'tests',
 		'vendor',
